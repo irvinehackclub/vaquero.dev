fca0f4d9c38914027ab5febea9fa39c2abe606fb
Yoda <yoda@yodacode.xyz>
2023-09-11 20:11:26 -0700

    changes

--- pages/api/interest.js
+++ pages/api/interest.js
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 
 export default async function handler(req, res) {
+    if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed, must be POST' });
+
     await prisma.interest.create({
         data: {
             name: req.body.name,
