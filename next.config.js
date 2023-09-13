7c4066583fca6effbdbceb5bf2f102b5ea5dba62
Yoda <yoda@yodacode.xyz>
2023-09-12 22:48:45 -0700

    remind

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: [
+    {
+      source: "/remind",
+      destination: "https://www.remind.com/join/ihs-hack",
+      permanent: true
+    }
+  ]
 }
 
 module.exports = nextConfig
