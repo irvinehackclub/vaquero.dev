c9dd9b1a769eff3a00225f77b731da4682d0a848
Yoda <yoda@yodacode.xyz>
2023-09-17 01:16:36 -0700

    changes

--- pages/editor/sign-in/[[...index]].js
+++ pages/editor/sign-in/[[...index]].js
@@ -1,6 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
  
 const SignInPage = () => (
-  <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+  <>
+    <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+    <span style="display: none;" class="thisPageIsClerkSignInOrSignUp"></span>
+  </>
 );
 export default SignInPage;
--- pages/editor/sign-up/[[...index]].js
+++ pages/editor/sign-up/[[...index]].js
@@ -1,6 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
  
 const SignUpPage = () => (
-  <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+  <>
+    <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+    <span style="display: none;" class="thisPageIsClerkSignInOrSignUp"></span>
+  </>
 );
 export default SignUpPage;
