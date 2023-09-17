2cd77845fdfccf6b09addb4b570420ff26b073e9
Yoda <yoda@yodacode.xyz>
2023-09-17 01:25:49 -0700

    classname

--- pages/editor/sign-in/[[...index]].js
+++ pages/editor/sign-in/[[...index]].js
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 const SignInPage = () => (
   <>
     <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
-    <span style={{ display: 'none' }} class="thisPageIsClerkSignInOrSignUp"></span>
+    <span style={{ display: 'none' }} className="thisPageIsClerkSignInOrSignUp"></span>
   </>
 );
 export default SignInPage;
--- pages/editor/sign-up/[[...index]].js
+++ pages/editor/sign-up/[[...index]].js
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 const SignUpPage = () => (
   <>
     <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-    <span style="display: none;" class="thisPageIsClerkSignInOrSignUp"></span>
+    <span style={{ display: 'none' }} className="thisPageIsClerkSignInOrSignUp"></span>
   </>
 );
 export default SignUpPage;
