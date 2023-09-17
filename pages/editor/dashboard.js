b8edf8b409f765f4af7c4fb78df0971dec06bb5a
Yoda <yoda@yodacode.xyz>
2023-09-17 02:15:10 -0700

    changes

--- pages/editor/dashboard.js
+++ pages/editor/dashboard.js
@@ -6,7 +6,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react'
 import Editor, { Monaco } from "@monaco-editor/react";
-import VictorMono from '@/components/VictorMono'
 
 
 export function Navbar () {
@@ -48,6 +47,11 @@ export function Code () {
 
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Victor+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+      </Head>
       <Editor
         height="40vh"
         defaultLanguage="javascript"
