e1d7e7f186e30e0a4e964502e580c3133f91c186
Yoda <yoda@yodacode.xyz>
2023-11-30 10:16:54 -0800

    Fix favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,18 @@ import '@/styles/globals.css'
 
 import { ClerkProvider } from '@clerk/nextjs'
 import { CssBaseline, GeistProvider } from '@geist-ui/core'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return <>
     <ClerkProvider>
       <GeistProvider themeType="dark">
         <CssBaseline />
+        <Head>
+          <link rel="favicon" href="/favicon.png" />
+          <link rel="icon" href="/favicon.png" />
+          <link rel="shortcut icon" href="/favicon.png" />
+        </Head>
         <Component {...pageProps} />
       </GeistProvider>
     </ClerkProvider>
