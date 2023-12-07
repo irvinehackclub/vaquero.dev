2c4886609024ad52706965a8baa00a9abe3c9464
Yoda <yoda@yodacode.xyz>
2023-12-07 15:22:27 -0800

    changes

--- pages/api/editor/code/api/v2/piston/execute.js
+++ pages/api/editor/code/api/v2/piston/execute.js
@@ -1,8 +1,10 @@
 export default async function execute (req, res) {
+    console.log(req.body);
     return await fetch('https://emkc.org/api/v2/piston/execute', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'User-Agent': 'insomnia/8.4.5'
         },
         body: JSON.stringify(req.body)
     }).then(r => r.text()).then(r => res.send(r));
