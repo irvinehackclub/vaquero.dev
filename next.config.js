9452931f9b2396243d12f6e3935d67c8272524c6
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2024-06-03 14:36:58 -0700

    Changes

--- next.config.js
+++ next.config.js
@@ -12,7 +12,21 @@ const nextConfig = {
       destination: "https://discord.gg/RcJpnuTDj6",
       permanent: true
     }
-  ]
+  ],
+  headers: async () => {
+    return [
+        {
+            // matching all API routes
+            source: "/api/editor/code/api/v2/piston/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" },
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+}
 }
 
 module.exports = nextConfig
