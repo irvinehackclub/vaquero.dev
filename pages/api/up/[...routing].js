c7ac547d6451c11d4c6cfff18f2d6ce866c3334a
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2024-09-08 00:13:21 -0700

    Changes

--- pages/api/up/[...routing].js
+++ pages/api/up/[...routing].js
@@ -29,7 +29,7 @@ export default async function handler (req, res) {
             new CodeExec.File(languages[obj.language].entryPoint, obj.files[0].content)
         )
     
-        res.json(result.output);
+        res.send(result.output);
 
     } catch (err) {
         console.error(err);
