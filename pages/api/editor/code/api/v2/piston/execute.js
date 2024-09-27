fc963f73766ba9c66a29b575f3cf9e012b13dbf8
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2024-09-26 17:55:34 -0700

    changes

--- pages/api/editor/code/api/v2/piston/execute.js
+++ pages/api/editor/code/api/v2/piston/execute.js
@@ -1,12 +1,30 @@
+import { issue } from "@/lib/api";
+
 export default async function execute (req, res) {
     console.log("INPUT:" + req.body);
+    const body = JSON.parse(req.body);
+    body.files = body.files.map(file => {
+        try {
+            if (file.name == "__vaquero__runtime.json") {
+                const json = JSON.parse(file.content);
+                file.content = JSON.stringify({
+                    ...json,
+                    apiKey: issue(json.project)
+                })
+            }
+        } catch (err) {
+            console.error("A", err);
+        }
+
+        return file;
+    });
     const output = await fetch('https://piston.ian.hackclub.app/api/v2/execute', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'User-Agent': 'insomnia/8.4.5'
         },
-        body: req.body
+        body: JSON.stringify(req.body)
     }).then(r => r.json());
 
     console.log("OUTPUT:", output);
