6b1d8765465a0e49f261580b8dcb7e0897e387bd
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2024-10-03 10:15:31 -0700

    kv

---
+++ pages/api/kv/get.js
@@ -0,0 +1,14 @@
+import { validate } from "@/lib/api";
+import { kv } from '@vercel/kv';
+
+export default async function handler(req, res) {
+    const project = validate(req.headers.authorization);
+    if (!project) return res.status(401).json({ error: "Unauthorized" });
+    
+    const { key } = req.query;
+    if (!key) return res.status(400).json({ error: "Missing key" });
+
+    res.status(200).json({
+        value: await kv.get(`${project}:${key}`)
+    });
+}
---
+++ pages/api/kv/set.js
@@ -0,0 +1,17 @@
+import { validate } from "@/lib/api";
+import { kv } from '@vercel/kv';
+
+export default async function handler(req, res) {
+    const project = validate(req.headers.authorization);
+    if (!project) return res.status(401).json({ error: "Unauthorized" });
+    
+    const { key } = req.query;
+    if (!key) return res.status(400).json({ error: "Missing key" });
+
+    const { value } = req.body;
+    if (!value) return res.status(400).json({ error: "Missing value" });
+
+    res.status(200).json({
+        result: await kv.set(`${project}:${key}`, value)
+    });
+}
