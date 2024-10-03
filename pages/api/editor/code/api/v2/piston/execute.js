b98a40b2abe840c684541489236653edfe6c4323
yodalightsabr <yoda@yodacode.xyz>
2024-10-03 09:31:41 -0700

    Update execute.js

--- pages/api/editor/code/api/v2/piston/execute.js
+++ pages/api/editor/code/api/v2/piston/execute.js
@@ -1,30 +1,34 @@
 import { issue } from "@/lib/api";
 
 export default async function execute (req, res) {
-    // console.log("INPUT:", req.body);
-    // const body = JSON.parse(req.body);
-    // body.files = body.files.map(file => {
-    //     try {
-    //         if (file.name == "__vaquero__runtime.json") {
-    //             const json = JSON.parse(file.content);
-    //             file.content = JSON.stringify({
-    //                 ...json,
-    //                 apiKey: issue(json.project)
-    //             })
-    //         }
-    //     } catch (err) {
-    //         console.error("A", err);
-    //     }
-
-    //     return file;
-    // });
+    try {
+        console.log("INPUT:", req.body);
+        const body = JSON.parse(req.body);
+        body.files = body.files.map(file => {
+            try {
+                if (file.name == "__vaquero__runtime.json") {
+                    const json = JSON.parse(file.content);
+                    file.content = JSON.stringify({
+                        ...json,
+                        apiKey: issue(json.project)
+                    })
+                }
+            } catch (err) {
+                console.error("A", err);
+            }
+    
+            return file;
+        });
+    } catch (err) {
+        console.error("B", err);
+    }
     const output = await fetch('https://piston.ian.hackclub.app/api/v2/execute', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'User-Agent': 'insomnia/8.4.5'
         },
-        body: JSON.stringify(req.body)
+        body: req.body
     }).then(r => r.json());
 
     console.log("OUTPUT:", output);
