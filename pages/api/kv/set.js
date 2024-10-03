e8a840335745ac89668305edbb1f352c38d38db3
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2024-10-03 10:18:19 -0700

    changes

--- pages/api/kv/set.js
+++ pages/api/kv/set.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     const project = validate(req.headers.authorization);
     if (!project) return res.status(401).json({ error: "Unauthorized" });
     
-    const { key } = req.query;
+    const { key } = req.params;
     if (!key) return res.status(400).json({ error: "Missing key" });
 
     const { value } = req.body;
