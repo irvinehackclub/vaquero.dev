d9b27a7534271f997d49619f7f8145444010f478
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2024-10-03 11:09:01 -0700

    changes

--- pages/api/kv/get.js
+++ pages/api/kv/get.js
@@ -4,8 +4,8 @@ import { kv } from '@vercel/kv';
 export default async function handler(req, res) {
     const project = validate(req.headers.authorization);
     if (!project) return res.status(401).json({ error: "Unauthorized" });
-    
-    const { key } = req.query;
+
+    const { key } = req.body;
     if (!key) return res.status(400).json({ error: "Missing key" });
 
     res.status(200).json({
